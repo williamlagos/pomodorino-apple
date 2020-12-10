15ec5f114998fd8f0637c29ba5f33ee0021d1f95
William Oliveira de Lagos <william.lagos@icloud.com>
2020-12-10 15:32:54 +0000

    Added new source code tree to the project folder

---
+++ Pomodorino/Pomodorino/PomodorinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodorinoApp.swift
+//  Pomodorino
+//
+//  Created by William Oliveira de Lagos on 10/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct PomodorinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
