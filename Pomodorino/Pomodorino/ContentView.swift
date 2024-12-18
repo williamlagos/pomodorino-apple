5303bd314ac971e46f52e183eb6435db52a25f75
William Oliveira de Lagos <william.lagos@icloud.com>
2024-12-18 11:42:13 +0100

    feat: changed content to get only the time in seconds and minutes for the interval in countdown

--- Pomodorino/Pomodorino/ContentView.swift
+++ Pomodorino/Pomodorino/ContentView.swift
@@ -6,30 +6,35 @@
 //
 
 import SwiftUI
+import Foundation
 
 let backgroundGradient = LinearGradient(
     colors: [Color.red, Color.yellow],
     startPoint: .top, endPoint: .bottom)
 
+func formatCountdown(seconds: Int) -> String {
+    // Calculate hours, minutes, and seconds
+    let hours = seconds / 3600
+    let minutes = (seconds % 3600) / 60
+    let remainingSeconds = seconds % 60
+    
+    // Format the result as "HH:mm:ss"
+    return String(format: "%02d:%02d:%02d", hours, minutes, remainingSeconds)
+}
+
 struct ContentView: View {
-    @State var currentDate = Date()
     @State var timeRemaining = 1500
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
         ZStack {
             backgroundGradient
             VStack {
-                Text("Countdown: \(timeRemaining)")
+                Text("Countdown: \(formatCountdown(seconds: timeRemaining))")
                     .padding()
                     .onReceive(timer) { _ in
                         if timeRemaining > 0 {
                             timeRemaining -= 1
                         }
-                    }
-                Text("\(currentDate)")
-                    .padding()
-                    .onReceive(timer) { input in
-                        currentDate = input
                     }
             }
         }
