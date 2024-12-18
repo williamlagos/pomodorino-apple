8939a2d4a9a28aa50ad4b03e5bf2d399c061feae
William Oliveira de Lagos <william.lagos@icloud.com>
2024-12-18 11:37:46 +0100

    feat: changed appearance on view by adding background and padding

--- Pomodorino/Pomodorino/ContentView.swift
+++ Pomodorino/Pomodorino/ContentView.swift
@@ -7,24 +7,33 @@
 
 import SwiftUI
 
+let backgroundGradient = LinearGradient(
+    colors: [Color.red, Color.yellow],
+    startPoint: .top, endPoint: .bottom)
+
 struct ContentView: View {
     @State var currentDate = Date()
     @State var timeRemaining = 1500
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
-        VStack {
-            Text("Countdown: \(timeRemaining)")
-                .bold()
-                .onReceive(timer) { _ in
-                    if timeRemaining > 0 {
-                        timeRemaining -= 1
+        ZStack {
+            backgroundGradient
+            VStack {
+                Text("Countdown: \(timeRemaining)")
+                    .padding()
+                    .onReceive(timer) { _ in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                        }
                     }
-                }
-            Text("\(currentDate)")
-                .onReceive(timer) { input in
-                    currentDate = input
-                }
+                Text("\(currentDate)")
+                    .padding()
+                    .onReceive(timer) { input in
+                        currentDate = input
+                    }
+            }
         }
+        .ignoresSafeArea()
     }
 }
 
